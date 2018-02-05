a245f1638a4b3e2798d93f530d1a05655fe61cdc
kurtulus86 <kurtuluskazan@gmail.com>
2018-02-05 15:54:23 +0300

    ilk

--- V/index.php
+++ V/index.php
@@ -5,10 +5,21 @@
  * Date: 5.02.2018
  * Time: 14:25
  */
-echo "<href src="lucas.jpg";
 ?>
-
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Giriş Ekrani</title>
+    <link rel="stylesheet" href="ana.css" type="text/css">
+</head>
+<body>
 <form action="C/ana.php" method="post">
-Kullanıcı Adı : <input type="text" name="kadi"/><br/>
+    Kullanıcı Adı : <input type="text" name="kadi"/><br/>
     Şifre : <input type="password" name="sifre"/><br/>
     <input type="submit" value="gönder">
+</body>
+</html>
