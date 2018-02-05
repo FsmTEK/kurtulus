65509345dd041f774b4188f3501541fae1f3b74c
kurtulus86 <kurtuluskazan@gmail.com>
2018-02-05 14:41:15 +0300

    ilk

--- V/index.php
+++ V/index.php
@@ -6,12 +6,8 @@
  * Time: 14:25
  */
 ?>
-<?php
+
 <form action="login.php method="post>
 Kullanıcı Adı : <imput type="text" name="username"/><br/>
     Şifre : <input type="password" name="password"/><br/>
     <input type="submit" value="gönder/">
-
-
-
-?>
