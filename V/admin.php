d8ddfcc94f8e020a4b32b8a885a8a63cf0094a5f
kurtulus86 <kurtuluskazan@gmail.com>
2018-02-06 16:26:39 +0300

    yükleme

---
+++ V/admin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucas
+ * Date: 6.02.2018
+ * Time: 16:25
+ */
+?>
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Paneli</title>
+</head>
+<body>
+Burası Admin Paneli
+</body>
+</html>
