7e3424ed235a2294a05463c2785efec07e2c8d77
kurtulus86 <kurtuluskazan@gmail.com>
2018-03-16 17:02:42 +0300

    like projesi

--- index.php
+++ index.php
@@ -47,7 +47,8 @@
         </a>
     </h4>
 </div>
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 </body>
 </html>
 
