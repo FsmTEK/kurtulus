91b80d0a1b2868913cd66f8c674e6c05020c4c88
kurtulus86 <kurtuluskazan@gmail.com>
2018-03-16 18:47:46 +0300

    like projesi

--- baglanti.php
+++ baglanti.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=like", "root", "");
+    $db = new PDO("mysql:host=localhost;dbname=like;charset=utf8", "root", "");
 } catch ( PDOException $e ){
     print $e->getMessage();
 }
