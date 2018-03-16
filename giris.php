2d1a9e33f8cdc5e6360c71e8d2a7cb1fd019e17e
kurtulus86 <kurtuluskazan@gmail.com>
2018-03-16 19:12:22 +0300

    like projesi

--- giris.php
+++ giris.php
@@ -1,9 +1,16 @@
 <?php
+include 'baglanti.php';
+
 /**
  * Created by PhpStorm.
  * User: Lucas
  * Date: 16.03.2018
  * Time: 18:51
  */
+if (isset($_POST['gonder'])){
+    echo "gonder dolu";
+
+}
+
 $gelen=$_POST;
 print_r($gelen);
