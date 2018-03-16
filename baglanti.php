6ff9951c0d37fb0d69a4fb63b02dbdbb3213d069
kurtulus86 <kurtuluskazan@gmail.com>
2018-03-16 18:32:49 +0300

    like projesi

---
+++ baglanti.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucas
+ * Date: 16.03.2018
+ * Time: 18:28
+ */
+
+$baglanti = mysql_connect("localhost", "root", "");
+if (!$baglanti)
+    exit("Bağlantı sağlanamadı.");
+
+mysql_select_db("like");
+
+mysql_close();
+
